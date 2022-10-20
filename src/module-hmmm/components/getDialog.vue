6b5731f8e7e118b482a732eb8203b3222dcbd068
潘一航 <11665009+pan-yihang@user.noreply.gitee.com>
2022-10-20 20:48:31 +0800

    权限和组题列表完成

---
+++ src/module-hmmm/components/getDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-dialog
+      :title="title"
+      :visible="visible"
+      width="50%"
+      :before-close="handleClose"
+    >
+      <slot name="header"></slot>
+      <slot name="body"> </slot>
+      <div><slot name="footer"></slot></div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      Number,
+    },
+  },
+  created() {
+    console.log(1);
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleClose() {
+      this.$emit("cancel");
+    },
+  },
+};
+</script>
+
+<style></style>
